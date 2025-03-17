a97d6c81ce9a9f2fb207373d7d3cdedff45975f7
labitayoub <ayoub.labite@gmail.com>
2025-03-17 03:21:29 +0000

    update

--- project/resources/views/layouts/main.blade.php
+++ project/resources/views/layouts/main.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>IC Book | @yield('title')</title>
+    <title>App School | @yield('title')</title>
     <link rel="icon" href="img/icons8-book-16.png" type="image/x-icon">
 
     <!-- Swiper CSS -->
