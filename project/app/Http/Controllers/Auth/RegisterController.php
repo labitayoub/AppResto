f5898b5279b4c2e4c5349e61214e1765beb6abe9
labitayoub <ayoub.labite@gmail.com>
2025-03-17 03:20:15 +0000

    update

--- project/app/Http/Controllers/Auth/RegisterController.php
+++ project/app/Http/Controllers/Auth/RegisterController.php
@@ -10,22 +10,12 @@ use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends Controller
 {
-    /**
-     * Show the registration form.
-     *
-     * @return \Illuminate\View\View
-     */
+
     public function showRegistrationForm()
     {
         return view('auth.register');
     }
 
-    /**
-     * Handle a registration request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\RedirectResponse
-     */
     public function register(Request $request)
     {
         // dd($request->all());
@@ -40,12 +30,6 @@ class RegisterController extends Controller
             ->with('success', 'Registration successful! Please login.');
     }
     
-    /**
-     * Get a validator for an incoming registration request.
-     *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
     protected function validator(array $data)
     {
         return Validator::make($data, [
@@ -55,13 +39,7 @@ class RegisterController extends Controller
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
     }
-    
-    /**
-     * Create a new user instance after a valid registration.
-     *
-     * @param  array  $data
-     * @return \App\Models\User
-     */
+
     protected function create(array $data)
     {
         return User::create([
